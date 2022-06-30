6fa814acd84917810cfa99c11e880ceb542382a2
Malte <malte.kluft@gmail.com>
2022-06-30 12:20:43 +0200

    ✨ Add login route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { loginService } from "../services/login.service";
 import { signupService } from "../services/signup.service";
 
 export async function authRoutes(fastify: FastifyInstance) {
   fastify.post("/auth/signup", signupService);
+  fastify.post("/auth/login", loginService);
 }
