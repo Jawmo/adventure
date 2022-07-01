f5d4fc2ada70dc10d383818ce45f89ae7db1393a
Malte <malte.kluft@gmail.com>
2022-07-01 14:42:37 +0200

    ✨ Add CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,25 @@
 import cookie from "@fastify/cookie";
+import cors from "@fastify/cors";
 import fastify from "fastify";
 
 import { authRoutes } from "./routes/auth.routes";
 
 const app = fastify({ logger: true });
+
 app.register(cookie, {
   secret: process.env.SECRET,
   parseOptions: {},
+});
+
+app.register(cors, {
+  origin: (origin, resolve) => {
+    const { hostname } = new URL(origin);
+    console.log(hostname);
+    if (hostname === "adventure.test") {
+      resolve(null, true);
+    }
+    resolve(new Error("Forbidden"), false);
+  },
 });
 
 app.register(authRoutes);
